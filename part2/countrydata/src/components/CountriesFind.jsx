cc792552740200ad16c10beda9efafe3e828bbfb
cristian <cristiancocabjn@gmail.com>
2025-08-08 21:37:50 -0400

    exercise: 2.19 step: 2

--- part2/countrydata/src/components/CountriesFind.jsx
+++ part2/countrydata/src/components/CountriesFind.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+import CountryInfo from "./CountryInfo"
+
 const CountriesFind = ({ countries }) => {
+    const [ selectedCountry, setSelectedCountry ] = useState('')
+
+    useEffect(() => {
+        setSelectedCountry('')
+    },[countries])
+
+    const selected = countries.find(c => c.cca3 === selectedCountry)
+
     return (
         <div>
-            {countries.map((c, index) => <p key={c.cca3}>{c.name.common}</p>)}
+            {countries.map(c =>
+                <p key={c.cca3}>
+                    {c.name.common}{' '}
+                    <button onClick={() => setSelectedCountry(c.cca3)}>Show</button>          
+                </p>
+            )}
+            {selected && <CountryInfo country={selected}/>}
         </div>
     )
 }
--- part2/countrydata/src/components/CountryInfo.jsx
+++ part2/countrydata/src/components/CountryInfo.jsx
@@ -2,7 +2,7 @@ const CountryInfo = ({ country }) => {
     return (
         <div>
             <h2>{country.name.common}</h2>
-            <p>Capital {country.capital.join(', ')}</p>
+            <p>Capital {country.capital?.join(', ')}</p>
             <p>Area {country.area}</p>
             <h2>Languages</h2>
             <LanguagesList list={country.languages}/>
